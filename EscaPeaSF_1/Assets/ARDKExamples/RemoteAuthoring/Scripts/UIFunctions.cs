b1807e85c4e78cd4630c48c7ccc4b148c16fbca0
billatgameology <101487342+billatgameology@users.noreply.github.com>
2022-12-18 16:48:44 -0800

    sunday end, no bugs

--- EscaPeaSF_1/Assets/ARDKExamples/RemoteAuthoring/Scripts/UIFunctions.cs
+++ EscaPeaSF_1/Assets/ARDKExamples/RemoteAuthoring/Scripts/UIFunctions.cs
@@ -9,59 +9,13 @@ using System;
 public class UIFunctions : MonoBehaviour
 {
     public Text transferTemp;
-    public GameObject ploppyItemMenu;
-    public RawImage playerIcon;
-    public Texture cloudTexture;
-    public Texture exploreTexture;
-    public Texture fountainTexture;
-    public Text currentObjectTxt;
-    public Button locate;
-
-
         private void Start()
     {
 
     }
-    public void onExplorePressed()
-    {
-        Debug.Log("explore puzzle pressed");
-        GameManager.Instance.puzzleID = 1;
-        transferTemp.text = 1.ToString();
-        ploppyItemMenu.SetActive(false);
-        playerIcon.texture = exploreTexture;
-        playerIcon.canvasRenderer.SetAlpha(1.0f);
-        currentObjectTxt.text = "Explore";
-        locate.gameObject.SetActive(true);
-    }
 
-    public void onCloudPressed()
-    {
-        Debug.Log("cloud puzzle pressed");
-        GameManager.Instance.puzzleID = 0;
-        transferTemp.text = 0.ToString();
-        ploppyItemMenu.SetActive(false);
-        playerIcon.texture = cloudTexture;
-        playerIcon.canvasRenderer.SetAlpha(1.0f);
-        currentObjectTxt.text = "Cloud";
-        locate.gameObject.SetActive(true);
-    }
 
-    public void onFountainPressed()
-    {
-        Debug.Log("Fountain puzzle pressed");
-        GameManager.Instance.puzzleID = 2;
-        transferTemp.text = 2.ToString();
-        ploppyItemMenu.SetActive(false);
-        playerIcon.texture = fountainTexture;
-        playerIcon.canvasRenderer.SetAlpha(1.0f);
-        currentObjectTxt.text = "Fountain";
-        locate.gameObject.SetActive(true);
-    }
 
-    public void onPloppyPressed()
-    {
-        ploppyItemMenu.SetActive(true);
-    }
 
     private void Update()
     {
