98c21b00d5138c8014ce2ee794f5815e842108e7
billatgameology <101487342+billatgameology@users.noreply.github.com>
2022-12-18 16:36:23 -0800

    map scene ui almost done
    still need map icon, interaction with map icon, play button

---
+++ EscaPeaSF_1/Assets/Samples/Lightship Maps SDK/1.0.0-beta2/Map-Based Simple Resource Game/Scripts/LocateButton.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Niantic.Lightship.Maps.Samples.GameSample;
+
+namespace Niantic.Lightship.Maps.Samples.GameSample
+{
+    public class LocateButton : MonoBehaviour
+    {
+        public Text transfer;
+        public GameObject playerHolder;
+        public GameObject playerIcon;
+        private PlayerLocationController playerLocationController;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerLocationController = playerHolder.GetComponent<PlayerLocationController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void onLocatePressed()
+        {
+            playerIcon.SetActive(false);
+            StartCoroutine(TurnBackOn());
+            
+
+            if (Convert.ToInt32(transfer.text) == 0)
+            {
+                Debug.Log("0");
+                playerLocationController.LocatePuzzle(37.79639f, -122.39814f); //cloud
+            }
+            else if (Convert.ToInt32(transfer.text) == 1)
+            {
+                Debug.Log("1");
+                playerLocationController.LocatePuzzle(37.800644999999996f, -122.398381f); //explore
+
+            }
+            else if (Convert.ToInt32(transfer.text) == 2)
+            {
+                Debug.Log("2");
+                playerLocationController.LocatePuzzle(37.797607f, -122.39887f); //fountain
+
+            }
+            else
+            {
+                Debug.Log("nothing selected");
+            }
+        }
+
+        IEnumerator TurnBackOn()
+        {
+            yield return new WaitForSeconds(5);
+            playerIcon.SetActive(true);
+            playerLocationController.GoBackAfterLocate();
+        }
+    }
+}
--- EscaPeaSF_1/Assets/Script/MapIconSpawner.cs
+++ EscaPeaSF_1/Assets/Script/MapIconSpawner.cs
@@ -8,10 +8,7 @@ public class MapIconSpawner : MonoBehaviour
     public GameObject puzzleExploratoruium_Icon;
     public GameObject puzzleCloudGate_Icon;
     public GameObject puzzleFourSeason_Icon;
-    public GameObject testingArroyoPark_Icon;
-    public GameObject testingQuarryLakesGarden_Icon;  //capsule
-    public GameObject testingNummi_Icon; // sphere
-    public GameObject testingEnsanada_Icon;  //Cylinder
+
 
 
     public LightshipMap _lightshipMap;
@@ -19,24 +16,16 @@ public class MapIconSpawner : MonoBehaviour
     private LatLng puzzleExploratorium_Location = new LatLng(37.800644999999996, -122.398381);
     private LatLng puzzleCloudGate_Location = new LatLng(37.79639, -122.39814);
     private LatLng puzzleFourSeason_Location = new LatLng(37.797607, -122.39887);
-    private LatLng testingArroyoPark_Location = new LatLng(37.581547, -122.01407);
-    private LatLng testingQuarryLakesGarden_Location = new LatLng(37.57564, -122.00994);
-    private LatLng testingEnsanada_Location = new LatLng(37.57601, -122.00805);
-    private LatLng testingNummi_Location = new LatLng(37.575317, -122.00853);
+
     
     
     // Start is called before the first frame update
     void Start()
     {   
-        //puzzleExploratoruium_Icon.transform.Translate(_lightshipMap.LatLngToScene(in puzzleExploratorium_Location));
-        //puzzleCloudGate_Icon.transform.Translate(_lightshipMap.LatLngToScene(in puzzleCloudGate_Location));
+
         Instantiate(puzzleExploratoruium_Icon, _lightshipMap.LatLngToScene(in puzzleExploratorium_Location), Quaternion.identity);
         Instantiate(puzzleCloudGate_Icon, _lightshipMap.LatLngToScene(in puzzleCloudGate_Location), Quaternion.identity);
         Instantiate(puzzleFourSeason_Icon, _lightshipMap.LatLngToScene(in puzzleFourSeason_Location), Quaternion.identity);
-        Instantiate(testingArroyoPark_Icon, _lightshipMap.LatLngToScene(in testingArroyoPark_Location), Quaternion.identity);
-        Instantiate(testingQuarryLakesGarden_Icon, _lightshipMap.LatLngToScene(in testingQuarryLakesGarden_Location), Quaternion.identity);
-        Instantiate(testingEnsanada_Icon, _lightshipMap.LatLngToScene(in testingEnsanada_Location), Quaternion.identity);
-        Instantiate(testingNummi_Icon, _lightshipMap.LatLngToScene(in testingNummi_Location), Quaternion.identity);
 
     }
 }
